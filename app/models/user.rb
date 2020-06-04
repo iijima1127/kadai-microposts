73887c4f2a28479d36be45fc44b53f5587ec9064
masaki.iijima <canchomeeen@gmail.com>
2020-06-04 11:37:18 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,30 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
+  #user_idとfollow_idのrelationship中間テーブルを用いた紐付け
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  #フォロー・アンフォローを可能にするメソッド
+  #自分自身ではないk、すでにフォローしているか
+  
+  def follow(other_user)
+     unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+     end
+  end
+  
+   def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+  
+  def following?(other_user)
+     self.followings.include?(other_user)
+  end
+   
 end
+
